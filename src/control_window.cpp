6d7f87c23f543797e1f84f0ad460f5db669f78d0
super mario <mario160@mail.ru>
2023-04-28 16:51:49 +0300

    Working edges view

--- src/control_window.cpp
+++ src/control_window.cpp
@@ -2,6 +2,10 @@
 
 #include "imgui.h"
 #include "imgui_internal.h"
+#include "misc/cpp/imgui_stdlib.h"
+
+#include <vector>
+#include <string>
 
 #include "vec2.h"
 
@@ -18,6 +22,20 @@ void display_control_window(Field& field) {
         ImGui::TableHeadersRow();
 
         const auto& graphs = field.get_graphs();
+
+        static vector<vector<string>> input_buffer(graphs.size());
+        static int init_buffer = [&](){
+            for(int g = 0; g < graphs.size(); g++){
+                const int m = graphs[g].edges.size();
+                input_buffer[g].resize(2 * m);
+                for(int i = 0; i < m; i++){
+                    input_buffer[g][2*i] = to_string(graphs[g].edges[i].first);
+                    input_buffer[g][2*i + 1] = to_string(graphs[g].edges[i].second);
+                }
+            }
+            return 0;
+        } ();
+
         for(int g = 0; g < graphs.size(); g++){
             ImGui::TableNextRow();
             ImGui::TableNextColumn();
@@ -56,6 +74,26 @@ void display_control_window(Field& field) {
                 ImGui::TableNextRow();
                 ImGui::TableNextColumn();
                 ImGui::Text("Edges");
+                for(int i = 0; i < m; i++){
+                    ImGui::TableNextRow();
+                    ImGui::TableNextColumn();
+
+                    const float input_size = (ImGui::GetColumnWidth() - ImGui::CalcTextSize(" - ").x) / 2;
+                    static const auto add_input_text = [](int j, int g, const float input_size){
+                        ImGui::PushID(&input_buffer[g][j]);
+                        ImGui::PushItemWidth(input_size);
+                        ImGui::InputText("", &input_buffer[g][j], 
+                            ImGuiInputTextFlags_CharsDecimal);
+                        ImGui::PopID();
+                    };
+                    add_input_text(2*i, g, input_size);
+
+                    ImGui::SameLine();
+                    ImGui::Text(" - ");
+                    
+                    ImGui::SameLine();
+                    add_input_text(2*i + 1, g, input_size);
+                }
 
                 ImGui::TreePop();
             }
