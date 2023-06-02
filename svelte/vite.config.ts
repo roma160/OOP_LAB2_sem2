05e62a973e9a1e07c95fc7effcde533c83fbc8ed
super mario <mario160@mail.ru>
2023-06-02 13:24:28 +0300

    Folder cleanup

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	
+	// https://medium.com/mkdir-awesome/two-easy-ways-to-publish-your-svelte-project-on-github-pages-c8eaca2b6225#c71c
+	build: {
+		outDir: "../docs/",
+		emptyOutDir: false
+	}
 });
